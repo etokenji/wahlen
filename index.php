f1258d6001ca2ca3f2c3a7f5a59566e086e980de
etokenji <ek14194@gmail.com>
2020-02-27 01:11:29 +0900

    ・home画面動的サイズ変更 ・スライド実装

---
+++ index.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html lang = "ja">
+<header>
+    <link rel="stylesheet" type="text/css" href="css\normalize.css"></link>
+    <link rel="stylesheet" type="text/css" href="css\style.css"></link>
+</header>
+    <body class="home">
+        <header>
+            <div class="container">
+                <nav class="home-header-base">
+                    <ul>
+                        <li class="home-header" style="margin-left:0px;">
+                            <a href="#">Home</a>
+                        </li>
+                        <li class="home-header" href="#">
+                            <a href="#">News</a>
+                        </li>
+                        <li class="home-header" href="#">
+                            <a href="#">Menu</a>
+                        </li>
+                        <li class="home-header" href="#">
+                            <a href="#">Access</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+        <nav class="photo-panel-wrap" id="photoPanelWrap">
+            <ul class="photo-panel photo-panel-1" id="photo1"></ul>
+            <ul class="photo-panel photo-panel-2" id="photo2"></ul>
+            <ul class="photo-panel photo-panel-3" id="photo3"></ul>
+            <ul class="photo-panel photo-panel-4" id="photo4"></ul>
+            <ul class="photo-panel photo-panel-5" id="photo5"></ul>
+
+        </nav>
+        
+        <div class="photo-panel-base"></div>
+    </body>
+    <script src = "lib/slider.js"></script>
+</html>
